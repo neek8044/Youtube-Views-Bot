f47f92337cade4d9d4cc973ea3bb3bb252645cd1
Nick Roussis <52631245+neek8044@users.noreply.github.com>
2022-09-14 22:07:53 +0300

    update installer.py
    add support for edge browser (kind of)

--- source/installer.py
+++ source/installer.py
@@ -1,44 +1,62 @@
 import os
 import requests
 import zipfile
-import tkinter as tk
+import webbrowser
+import tkinter as ttk
 import tkinter.messagebox as msg
 
-win = tk.Tk()
+
+win = ttk.Tk()
 win.resizable(0,0)
 win.title('Installer')
 win.geometry('280x130')
 
 cwd = os.getcwd() + '\\'
 
+
 # Display message when installed
-def message():
-    msg.showinfo(title='Installer', message='Chromedriver installed in the local folder.')
+def message(browser:str):
+    msg.showinfo(title='Installer', message='%sdriver installed in the local folder.' % browser)
+
 
 # Call all the functions above
 def setup():
-    with zipfile.ZipFile(cwd + 'chromedriver_win32.zip', 'r') as zipped:
+    with zipfile.ZipFile(cwd + 'chromedriver_latest.zip', 'r') as zipped:
         zipped.extractall(cwd)
-    message()
+    message("Chrome")
 
-# Request driver 101
-def get_latest():
+
+# Request chrome driver
+def chrome():
     # get the latest chrome driver version number
     url = 'https://chromedriver.storage.googleapis.com/LATEST_RELEASE'
     response = requests.get(url)
     version_number = response.text
     
     # build the download url
-    download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_win32.zip"
+    download_url = "https://chromedriver.storage.googleapis.com/" + version_number +"/chromedriver_latest.zip"
     
     # download and extract the zip file
     driver_zip = requests.get(download_url)
-    open('chromedriver_win32.zip', 'wb').write(driver_zip.content)
+    open('chromedriver_latest.zip', 'wb').write(driver_zip.content)
     setup()
 
+
+# Request edge driver
+def edge():
+    # open new tab
+    webbrowser.open("https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/")
+
 # Display window and buttons
-lbl_1 = tk.Label(text='Download driver:').pack(pady=(4,0))
-btn_1 = tk.Button(text='Get Latest', fg='white', bg='#108010', command=get_latest, padx=18, pady=6).pack(pady=1)
-lbl_2 = tk.Label(text='Created by Neek8044', fg='magenta', pady=4).pack(side='bottom')
+lbl_1 = ttk.Label(text = 'Download driver:')
+lbl_1.pack(pady = (4,0))
+
+btn_1 = ttk.Button(text = 'Chrome [auto download]', fg = 'white', bg = '#30b030', command = chrome, padx = 18, pady = 6)
+btn_1.pack(pady = 1)
+btn_1 = ttk.Button(text = 'Edge [open link]', fg = 'white', bg = '#30b030', command = edge, padx = 18, pady = 6)
+btn_1.pack(pady = 1)
+
+lbl_2 = ttk.Label(text = 'Created by Neek8044', fg = 'magenta', pady = 4)
+lbl_2.pack(side = 'bottom')
 
 win.mainloop()
